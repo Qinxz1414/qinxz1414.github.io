fa1331a3649f3f114e7a27eaf82e6ebeb147e45c
Qinxz1414 <qinxz1414@qq.com>
2020-10-12 03:05:43 +0800

    commit

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?af385a6dde9240d862621984ea8b6974";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
